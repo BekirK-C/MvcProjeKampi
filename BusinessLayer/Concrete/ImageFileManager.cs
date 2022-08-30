8aba0a5eedad48034c38023974524d805fe7d27a
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-08-30 20:23:46 +0300

    Image Files

---
+++ BusinessLayer/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+        IImageFileDal _İmagefileDal;
+
+        public ImageFileManager(IImageFileDal imagefileDal)
+        {
+            _İmagefileDal = imagefileDal;
+        }
+
+        public List<ImageFile> GetList()
+        {
+            return _İmagefileDal.List();
+        }
+    }
+}
