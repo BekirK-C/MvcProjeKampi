977b2aa003285e7ee9b07d1a7a252dff12965c0b
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-09-01 18:23:04 +0300

    Admin Roles

---
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            Context c = new Context();
+            var adminUserInfo = c.Admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName && x.AdminPassword == p.AdminPassword);
+            if (adminUserInfo != null)
+            {
+                FormsAuthentication.SetAuthCookie(adminUserInfo.AdminUserName,false);
+                Session["AdminUserName"] = adminUserInfo.AdminUserName;
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                //Response.Write("<script language='javascript'>alert(\"Hatalı Kullanıcı Adı veya Şifre Girdiniz\")</script>");
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
