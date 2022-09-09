e950611eb4918f1e0af6ef1c6586625059155448
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-09-09 15:03:36 +0300

    Report process

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -19,8 +19,12 @@ namespace MvcProjeKampi.Controllers
         {
             var headingvalues = hm.GetList();
             return View(headingvalues);
+        }public ActionResult HeadingReport()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
         }
-
+        
         [HttpGet]
         public ActionResult AddHeading()
         {
