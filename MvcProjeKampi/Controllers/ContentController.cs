a9b6cd21668aa72a533c2f696f3b7a9bdfa13ae2
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-09-09 11:09:57 +0300

    Search Task

--- MvcProjeKampi/Controllers/ContentController.cs
+++ MvcProjeKampi/Controllers/ContentController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,13 @@ namespace MvcProjeKampi.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetAllContent(string p = "")
+        {
+            var values = cm.GetList(p);
+            // var values = c.Contents.ToList();
+            return View(values);
         }
         public ActionResult ContentByHeading(int id)
         {
